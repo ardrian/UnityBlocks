52fb05159aff9e5458834bf9954197a778a3e335
Ardrian Hardjono <mail@ardrian.com>
2018-06-29 16:10:48 +0100

    Initial commit

---
+++ Assets/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    Vector3 originalPos;
+    Vector3 targetPos;
+    bool doorOpen; 
+
+    private void Start()
+    {
+        originalPos = transform.position;
+        targetPos = originalPos + new Vector3(0, 10, 0); 
+    }
+
+    private void Update()
+    {
+        if (doorOpen)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime); 
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Door trigger");
+        doorOpen = true; 
+
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    float moveSpeed = 5.0f;
+    float rotationSpeed = 10.0f;
+    bool openDoor = true; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        //Vector3 position = transform.position;
+        //transform.position = transform.position + transform.forward * moveY * moveSpeed * Time.deltaTime;
+
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y += rotationSpeed * moveX * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(rotation); 
+
+        Vector3 moveVector = transform.forward * moveY * moveSpeed;
+        GetComponent<CharacterController>().SimpleMove(moveVector);
+    }
+
+    
+}
